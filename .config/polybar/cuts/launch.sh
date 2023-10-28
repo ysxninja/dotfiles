267cab51a1c662c0e4bd5a6d2cda9edd1f052ed1
ya-suke <fredluki100@gmail.com>
2023-10-28 09:10:58 +0300

    fix dock: better workspace move in i3

--- .config/polybar/cuts/launch.sh
+++ .config/polybar/cuts/launch.sh
@@ -1,19 +1,5 @@
 #!/usr/bin/env bash
 
-# Specify the location of the pre-i3 startup script
-# Retrieve value of $FM
-PRE_I3_SCRIPT="/home/yasuke/.local/bin/pre_i3_startup.sh"
-
-# Check if the pre-i3 script exists and is readable
-if [ -f "$PRE_I3_SCRIPT" ] && [ -r "$PRE_I3_SCRIPT" ]; then
-    # Source the pre-i3 startup script to set FM variable
-    # shellcheck disable=1090
-    source "$PRE_I3_SCRIPT"
-else
-    echo "Error: Pre-i3 startup script not found or not readable"
-    exit 1
-fi
-
 # Add this script to your wm startup file.
 
 DIR="$HOME/.config/polybar/cuts"
@@ -26,7 +12,7 @@ polybar-msg cmd quit
 
 # The $FM variable is either VGA1(if on) or LVDS1(default)
 laptop=LVDS1
-vga=VGA1
+external1=VGA1
 vga_status=$(xrandr | grep "VGA1 connected")
 
 # Touch temp log files / create if they dont exist
@@ -34,13 +20,11 @@ echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 
 if [ -n "$vga_status" ]; then
   # VGA monitor is connected
-  # # Launch external-bar and laptop-bar for dual monitor setup
+  # Launch external-bar and laptop-bar for dual monitor setup
   # Xrandr organise displays, FM(VGA1) to the right of LVDS1
-  xrandr --output $laptop --auto --right-of "$FM"
+  xrandr --output $laptop --auto --right-of "$external1"
 
   # Move workspaces to FM (VGA1)
-  i3-msg "workspace 1, move workspace to output $FM"
-  i3-msg "workspace 2, move workspace to output $FM"
   polybar -q external-bar -c "$DIR"/config.ini 2>&1 | tee -a /tmp/polybar1.log & disown
   polybar -q laptop-bar -c "$DIR"/config.ini 2>&1 | tee -a /tmp/polybar2.log & disown
 
@@ -50,11 +34,9 @@ if [ -n "$vga_status" ]; then
 
 else
   # VGA monitor is disconnected, move workspaces to FM(LVDS1)
-  # # Launch laptopwithout-bar for single monitor setup
+  # Launch laptopwithout-bar for single monitor setup
   # Configure monitors and move workspaces to FM(LVDS1)
-  xrandr --output "$laptop" --auto --primary --output $vga --off
-  i3-msg "workspace 1, move workspace to output $FM"
-  i3-msg "workspace 2, move workspace to output $FM"
+  xrandr --output $laptop --auto --primary --output $external1 --off
   polybar -q laptopwithout-bar -c "$DIR"/config.ini 2>&1 | tee -a /tmp/polybar2.log & disown
 
   # Set random wallpaper
