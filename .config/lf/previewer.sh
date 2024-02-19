552787e3855bf0b9abd2dbd14394fc7834f0b1db
ya-suke <fredluki100@gmail.com>
2024-02-19 21:12:10 +0000

    update lf with ueberzugpp for wayland

--- .config/lf/previewer.sh
+++ .config/lf/previewer.sh
@@ -1,65 +1,64 @@
 #!/bin/sh
-draw() {
-  ~/.config/lf/draw_img.sh "$@"
-  exit 1
+
+image() {
+    FILE_PATH="$1"
+    X=$(($4*70/100))
+    Y=$5
+    MW=$(($2*60/100))
+    MH=$3
+    ueberzugpp cmd -s "$UB_SOCKET" -a add -i PREVIEW -x "$X" -y "$Y" --max-width "$MW" --max-height "$MH" -f "$FILE_PATH"
+    exit 1
 }
 
-hash() {
-  printf '%s/.cache/lf/%s' "$HOME" \
-    "$(stat --printf '%n\0%i\0%F\0%s\0%W\0%Y' -- "$(readlink -f "$1")" | sha256sum | awk '{print $1}')"
+batorcat() {
+    file="$1"
+    shift
+    if command -v bat >/dev/null 2>&1; then
+        bat --color=always --style=plain --pager=never "$file" "$@"
+    else
+        cat "$file"
+    fi
 }
 
-cache() {
-  if [ -f "$1" ]; then
-    draw "$@"
-  fi
-}
+CACHE="$HOME/.cache/lf/thumbnail.$(stat --printf '%n\0%i\0%F\0%s\0%W\0%Y' -- "$(readlink -f "$1")" | sha256sum | awk '{print $1}'))"
 
-file="$1"
-shift
-
-if [ -n "$FIFO_UEBERZUG" ]; then
-  case "$(file -Lb --mime-type -- "$file")" in
-    image/*)
-      orientation="$(identify -format '%[EXIF:Orientation]\n' -- "$file")"
-      if [ -n "$orientation" ] && [ "$orientation" != 1 ]; then
-        cache="$(hash "$file").jpg"
-        cache "$cache" "$@"
-        convert -- "$file" -auto-orient "$cache"
-        draw "$cache" "$@"
-      else
-        draw "$file" "$@"
-      fi
-      ;;
-    video/*)
-      cache="$(hash "$file").jpg"
-      cache "$cache" "$@"
-      ffmpegthumbnailer -i "$file" -o "$cache" -s 0
-      draw "$cache" "$@"
-      ;;
-    audio/*)
-      exiftool "$file"
-      ;;
-    application/x-tar)
-      tar tf "$file"
-      ;;
-    application/zip)
-      unzip -l "$file"
-      ;;
-    application/x-rar-compressed)
-      unrar l "$file"
-      ;;
-    application/x-7z-compressed)
-      7z l "$file"
-      ;;
-    application/pdf)
-      exiftool "$file"
-      ;;
+case "$(printf "%s\n" "$(readlink -f "$1")" | tr '[:upper:]' '[:lower:]')" in
+    *.tgz | *.tar.gz) tar tzf "$1" ;;
+    *.tar.bz2 | *.tbz2) tar tjf "$1" ;;
+    *.tar.txz | *.txz) xz --list "$1" ;;
+    *.tar) tar tf "$1" ;;
+    *.zip | *.jar | *.war | *.ear | *.oxt) unzip -l "$1" ;;
+    *.rar) unrar l "$1" ;;
+    *.7z) 7z l "$1" ;;
+    *.[1-8]) man "$1" | col -b ;;
+    *.o) nm "$1" ;;
+    *.torrent) transmission-show "$1" ;;
+    *.iso) iso-info --no-header -l "$1" ;;
+    *.odt | *.ods | *.odp | *.sxw) odt2txt "$1" ;;
+    *.doc) catdoc "$1" ;;
+    *.docx) docx2txt "$1" - ;;
+    *.xls | *.xlsx)
+        ssconvert --export-type=Gnumeric_stf:stf_csv "$1" "fd://1" | batorcat --language=csv
+        ;;
+    *.wav | *.mp3 | *.flac | *.m4a | *.wma | *.ape | *.ac3 | *.og[agx] | *.spx | *.opus | *.as[fx] | *.mka)
+        exiftool "$1"
+        ;;
+    *.pdf)
+        exiftool "$1"
+        ;;
+    *.avi | *.mp4 | *.wmv | *.dat | *.3gp | *.ogv | *.mkv | *.mpg | *.mpeg | *.vob | *.fl[icv] | *.m2v | *.mov | *.webm | *.ts | *.mts | *.m4v | *.r[am] | *.qt | *.divx)
+        [ ! -f "${CACHE}.jpg" ] && ffmpegthumbnailer -i "$1" -o "${CACHE}.jpg" -s 0 -q 5
+        image "${CACHE}.jpg" "$2" "$3" "$4" "$5"
+        ;;
+    *.bmp | *.jpg | *.jpeg | *.png | *.xpm | *.webp | *.gif | *.jfif)
+        image "$1" "$2" "$3" "$4" "$5"
+        ;;
+    *.svg)
+        [ ! -f "${CACHE}.jpg" ] && convert "$1" "${CACHE}.jpg"
+        image "${CACHE}.jpg" "$2" "$3" "$4" "$5"
+        ;;
     *)
-      bat --color=always --style=plain --pager=never "$file" || highlight --out-format ansi "$file"
-      ;;
-  esac
-fi
-
-file -Lb -- "$1" | fold -s -w "$width"
+        batorcat "$1"
+        ;;
+esac
 exit 0
