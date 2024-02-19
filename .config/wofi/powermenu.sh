6144c4399b59d12bb807d182dbfa9c1505135b30
ya-suke <fredluki100@gmail.com>
2024-02-19 19:39:22 +0000

    update wofi powermenu

--- .config/wofi/powermenu.sh
+++ .config/wofi/powermenu.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-entries=" Lock\n󰍃 Logout\n Suspend\n Reboot\n Shutdown"
+entries=" Shutdown\n Suspend\n Reboot\n󰍃 Logout\n Lock"
 
 selected=$(echo -e "$entries" | wofi --conf="$HOME"/.config/wofi/config-powermenu | awk '{print tolower($1)}')
 
 case $selected in
   0)
-    swaylock;;
-  1)
-    loginctl terminate-user "$(whoami)";;
-  2)
-    exec systemctl suspend;;
-  3)
-    exec systemctl reboot;;
-  4)
     exec systemctl poweroff -i;;
+  1)
+    exec systemctl suspend;;
+  2)
+    exec systemctl reboot;;
+  3)
+    loginctl terminate-user "$(whoami)";;
+  4)
+    swaylock;;
 esac
