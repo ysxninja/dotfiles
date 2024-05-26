2b7d57319dbc34c4b6d289868d44a070dc771bbf
ysxninja <50769646+ysxninja@users.noreply.github.com>
2024-05-26 16:33:32 +0300

    Update aliases.zsh

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,5 +1,9 @@
 ## ALIASES
 
+# colors and stuff
+alias ls="ls --color=auto"
+alias grep="grep --color=auto"
+
 # lf with ueberzug
 alias lf="lfub"
 
@@ -7,6 +11,33 @@ alias lf="lfub"
 alias h="history"
 alias hs="history | grep"
 alias hsi="history | grep -i"
+
+# adding flags
+alias df='df -h'                          # human-readable sizes
+alias free='free -m'                      # show sizes in MB
+alias vifm='~/.config/vifm/scripts/vifmrun'
+
+## get top process eating memory
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+
+## get top process eating cpu ##
+alias pscpu='ps auxf | sort -nr -k 3'
+alias pscpu10='ps auxf | sort -nr -k 3 | head -10'
+
+# get error messages from journalctl
+alias jctl="journalctl -p 3 -xb"
+
+# switch between shells
+alias tobash="chsh $USER -s /bin/bash && echo 'Now log out.'"
+alias tozsh="chsh $USER -s /bin/zsh && echo 'Now log out.'"
+alias tofish="chsh $USER -s /bin/fish && echo 'Now log out.'"
+
+# bare git repo alias for dotfiles
+alias dotgit="/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
+
+# termbin
+alias tb="nc termbin.com 9999"
 
 ### Function extract for common file formats ###
 SAVEIFS=$IFS
@@ -52,30 +83,3 @@ fi
 IFS=$SAVEIFS
 
 ######
-
-# adding flags
-alias df='df -h'                          # human-readable sizes
-alias free='free -m'                      # show sizes in MB
-alias vifm='~/.config/vifm/scripts/vifmrun'
-
-## get top process eating memory
-alias psmem='ps auxf | sort -nr -k 4'
-alias psmem10='ps auxf | sort -nr -k 4 | head -10'
-
-## get top process eating cpu ##
-alias pscpu='ps auxf | sort -nr -k 3'
-alias pscpu10='ps auxf | sort -nr -k 3 | head -10'
-
-# get error messages from journalctl
-alias jctl="journalctl -p 3 -xb"
-
-# switch between shells
-alias tobash="chsh $USER -s /bin/bash && echo 'Now log out.'"
-alias tozsh="chsh $USER -s /bin/zsh && echo 'Now log out.'"
-alias tofish="chsh $USER -s /bin/fish && echo 'Now log out.'"
-
-# bare git repo alias for dotfiles
-alias dotgit="/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
-
-# termbin
-alias tb="nc termbin.com 9999"
