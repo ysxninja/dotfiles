a649c52d395898488858c1befcfdaf4acffcf0d2
ya-suke <fredluki100@gmail.com>
2024-10-16 21:08:22 +0300

    add and configure notifications with swaync

--- .config/hypr/scripts/pick_wallpaper.sh
+++ .config/hypr/scripts/pick_wallpaper.sh
@@ -6,3 +6,5 @@ conf_dir=$HOME/.config/hypr
 random_pick=$(find "$wp_dir" -type f -name "*.png" -o -name "*.jpg" | shuf -n1)
 # Replace the placeholder in template and save to .conf file
 sed -e "s~<wp>~${random_pick}~g" "$conf_dir"/scripts/hyprpaper.template > "$conf_dir"/hyprpaper.conf
+# update terminal colors with pywal
+wal -i "$random_pick"
--- .config/hypr/scripts/reload.sh
+++ .config/hypr/scripts/reload.sh
@@ -2,10 +2,12 @@
 
 killall waybar
 killall hyprpaper
+killall swaync
 kdeconnect-cli --refresh
 
 "$HOME"/.config/hypr/scripts/pick_wallpaper.sh
 waybar &
 hyprpaper &
+swaync &
 
 hyprctl reload
