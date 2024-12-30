cbbb5ae4757448a5d1a0e3d4e125f4af9614711f
ya-suke <fredluki100@gmail.com>
2024-12-30 14:04:05 +0300

    update dotfiles

--- .config/hypr/scripts/pick_wallpaper.sh
+++ .config/hypr/scripts/pick_wallpaper.sh
@@ -3,7 +3,8 @@
 wp_dir=$HOME/wallpapers
 conf_dir=$HOME/.config/hypr
 # Pick random wallpaper
-random_pick=$(find "$wp_dir" -type f -name "*.png" -o -name "*.jpg" | shuf -n1)
+# random_pick=$(find "$wp_dir" -type f -name "*.png" -o -name "*.jpg" | shuf -n1)
+random_pick=$HOME/wallpapers/goku-musculoso-dragon-ball-z-amoled-minimal-black-1920x1200-4945.png
 # Replace the placeholder in template and save to .conf file
 sed -e "s~<wp>~${random_pick}~g" "$conf_dir"/scripts/hyprpaper.template > "$conf_dir"/hyprpaper.conf
 # update terminal colors with pywal
