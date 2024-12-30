bf21db0ea11a5bdab998160b2cdf7c2c292e62b9
ya-suke <fredluki100@gmail.com>
2024-12-30 15:10:48 +0300

    update hyprland, autostart and reload script

--- .config/hypr/scripts/reload.sh
+++ .config/hypr/scripts/reload.sh
@@ -2,13 +2,9 @@
 
 killall waybar
 killall hyprpaper
-killall swaync
 killall wl-gammarelay
+
 kdeconnect-cli --refresh
 
-"$HOME"/.config/hypr/scripts/pick_wallpaper.sh
 waybar &
-hyprpaper &
-swaync &
-
 hyprctl reload
