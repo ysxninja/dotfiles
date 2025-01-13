d97cfb9c9ddb7933b5aebc6229c990bb155050ab
ya-suke <fredluki100@gmail.com>
2025-01-13 12:08:51 +0300

    update dotfiles

--- .config/hypr/scripts/reload.sh
+++ .config/hypr/scripts/reload.sh
@@ -2,8 +2,12 @@
 
 killall waybar
 killall bluelighttoggle
+killall hyprpaper
 
 kdeconnect-cli --refresh
 
-waybar &
+pidof waybar || nohup waybar >/dev/null 2>&1 &
+pidof hyprpaper || nohup hyprpaper >/dev/null 2>&1 &
 hyprctl reload
+
+exit 0
