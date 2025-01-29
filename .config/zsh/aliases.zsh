8baa92b16b2e6ec9185770446fabfe1c6e530eea
ya-suke <fredluki100@gmail.com>
2025-01-29 16:23:33 +0300

    Automated dotfiles update

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -71,7 +71,7 @@ alias tb="nc termbin.com 9999"
 
 # reflector
 # get fastest mirrors
-alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+alias mirror="sudo reflector -f 30 -l 30 --number 10 --download-timeout 60 --verbose --save /etc/pacman.d/mirrorlist"
 alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
 alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
 alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
