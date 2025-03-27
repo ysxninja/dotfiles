89628af3c254bbc4f81acdd619f5d03bc3132eb8
ya-suke <fredluki100@gmail.com>
2025-03-27 12:19:47 +0300

    Automated dotfiles update

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -30,13 +30,6 @@ alias vim="nvim"
 # bat
 alias cat='bat'
 
-# Changing "ls" to "eza"
-alias ls='eza -al --color=always --group-directories-first --icons -a -s type' # my preferred listing
-alias la='eza -a --color=always --group-directories-first --icons -a -s type'  # all files and dirs
-alias ll='eza -l --color=always --group-directories-first --icons -a -s type'  # long format
-alias lt='eza -aT --color=always --group-directories-first' # tree listing
-alias l.='eza -a | rg "^\."'
-
 # confirm before overwriting something
 alias cp='cp -i'
 alias mv='mv -i'
@@ -119,4 +112,47 @@ fi
 
 IFS=$SAVEIFS
 
-######
+##### Easier directory navigation #############
+
+# Changing/making/removing directory
+setopt auto_cd
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushdminus
+
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+alias -g ......='../../../../..'
+
+alias -- -='cd -'
+alias 1='cd -1'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
+alias 9='cd -9'
+
+alias md='mkdir -p'
+alias rd=rmdir
+
+function d () {
+  if [[ -n $1 ]]; then
+    dirs "$@"
+  else
+    dirs -v | head -n 10
+  fi
+}
+compdef _dirs d
+
+# List directory contents
+# Changing "ls" to "eza"
+alias ls='eza -al --color=always --group-directories-first --icons -a -s type' # my preferred listing
+alias la='eza -a --color=always --group-directories-first --icons -a -s type'  # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons -a -s type'  # long format
+alias lt='eza -aT --color=always --group-directories-first' # tree listing
+alias l.='eza -a | rg "^\."'
+##### Easier directory navigation #############
