11c112045ac3e8018eafad2cc2b094940c637085
ysxninja <fredluki100@gmail.com>
2025-09-12 15:16:34 +0300

    add new user services, update configs

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -2,6 +2,7 @@
 alias tt="taskwarrior-tui"
 alias su="su -"
 alias hyprfix='pacman -Qq | grep -E "(hypr|aqua)" | grep -- -git | yay -Sy --rebuild --rebuildall --rebuildtree --noconfirm -'
+alias neofetch="fastfetch"
 
 # lf with ueberzug, image previews better with kitty, using yazi
 # alias lf="lfub"
