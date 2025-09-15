e16d7c6d84324f9b92fb17586b958e04f0868f0f
ysxninja <50769646+ysxninja@users.noreply.github.com>
2025-09-15 13:07:20 +0300

    Update and rename vimsetup.sh to utilsetup.sh
    General Utils installer

--- utilsetup.sh
+++ utilsetup.sh
@@ -1,3 +1,5 @@
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
