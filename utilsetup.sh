57568a7d470edf9a44488e21bcf4528c4e96d8ad
ysxninja <50769646+ysxninja@users.noreply.github.com>
2025-09-15 13:14:06 +0300

    Add zsh to package installation in utilsetup.sh

--- utilsetup.sh
+++ utilsetup.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-sudo apt-get install gawk net-tools coreutils tmux
+sudo apt-get install gawk net-tools coreutils tmux zsh
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
