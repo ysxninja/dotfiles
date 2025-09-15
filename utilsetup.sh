f4448e1b7528e02fe5301d14bacc80ae342a00f7
ysxninja <50769646+ysxninja@users.noreply.github.com>
2025-09-15 13:12:37 +0300

    Add package installation to utilsetup.sh

--- utilsetup.sh
+++ utilsetup.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+sudo apt-get install gawk net-tools coreutils tmux
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
